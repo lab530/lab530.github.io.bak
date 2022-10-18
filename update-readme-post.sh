7f4082d889d49dcf7fe31bdaf9e46bd80330b447
Ivan Chien <qyc027@gmail.com>
2022-10-18 22:58:22 +0800

    feat!: add subtitle
    - add feature codes
    - update for all posts' subtitles
    - update `update-readme-post.sh`

--- update-readme-post.sh
+++ update-readme-post.sh
@@ -6,12 +6,13 @@ git rm _posts/*-readme.md
 { echo '---
 layout: post
 title:  How to Work with Us - lab530.github.io 开发手册
+subtitle: 如何和我们合作
 author: ivan_chien
 categories: [ 开发 ]
 tags: [ 开发 ]
 ref: https://github.com/lab530/lab530.github.io/blob/main/readme.md
 ---';
-echo "Updated at $(date) by \`update-readme-post.sh\`";
+echo -e "\nUpdated at $(date) by \`update-readme-post.sh\`";
 tail -n +2 readme.md; } > $filename
 
 git add $filename
