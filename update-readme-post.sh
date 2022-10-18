60adab028121cc131f68b3ad06e84eb1727b966a
Ivan Chien <qyc027@gmail.com>
2022-10-18 22:37:14 +0800

    feat: add auto update readme in post script

---
+++ update-readme-post.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+filename="_posts/$(date -Idate)-readme.md"
+
+git rm _posts/*-readme.md
+{ echo '---
+layout: post
+title:  How to Work with Us - lab530.github.io 开发手册
+author: ivan_chien
+categories: [ 开发 ]
+tags: [ 开发 ]
+---'; tail -n +2 readme.md; } > $filename
+
+git add $filename
+git commit -m "script: update readme in post"
